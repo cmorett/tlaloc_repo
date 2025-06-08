5545ce139f0fb44831ecfd73ee96df2cdca5e4e8
cmorett <cmorettld@gmail.com>
2025-06-08 12:05:20 -0600

    imported nn

--- scripts/mpc_control.py
+++ scripts/mpc_control.py
@@ -8,6 +8,7 @@ from pathlib import Path
 import numpy as np
 import pandas as pd
 import torch
+import torch.nn as nn
 from torch_geometric.nn import GCNConv
 import wntr
 from wntr.metrics.economic import pump_energy
