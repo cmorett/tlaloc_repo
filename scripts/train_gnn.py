9838be75256b3558e22345c79ee8d3a5537b8e61
cmorett <cmorettld@gmail.com>
2025-07-08 09:36:23 -0600

    changed head weights

--- scripts/train_gnn.py
+++ scripts/train_gnn.py
@@ -2420,7 +2420,7 @@ if __name__ == "__main__":
     parser.add_argument(
         "--w_head",
         type=float,
-        default=0.5,
+        default=0.005,
         help="Weight of the head loss consistency term",
     )
     parser.add_argument(
