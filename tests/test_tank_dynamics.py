37566a8cb98d877af4204b07033ee77763b30d49
cmorett <cmorettld@gmail.com>
2025-09-20 11:41:39 -0600

    Fix tank net flow double counting

--- tests/test_tank_dynamics.py
+++ tests/test_tank_dynamics.py
@@ -1,9 +1,22 @@
 import torch
+from torch import nn
+
 from scripts.train_gnn import MultiTaskGNNSurrogate
 
+
+class ConstantEdgeDecoder(nn.Module):
+    def __init__(self, flows):
+        super().__init__()
+        self.register_buffer("flows", torch.tensor(flows, dtype=torch.float32))
+
+    def forward(self, edge_emb):
+        flows = self.flows.to(edge_emb.device, dtype=edge_emb.dtype)
+        flows = flows.view(1, 1, -1, 1)
+        return flows.repeat(edge_emb.shape[0], edge_emb.shape[1], 1, 1)
+
 def test_tank_pressure_update():
-    edge_index = torch.tensor([[0],[1]], dtype=torch.long)
-    edge_attr = torch.ones(1,10)
+    edge_index = torch.tensor([[0,1],[1,0]], dtype=torch.long)
+    edge_attr = torch.ones(2,10)
     model = MultiTaskGNNSurrogate(
         in_channels=2,
         hidden_channels=4,
@@ -20,12 +33,11 @@ def test_tank_pressure_update():
     # constant decoders so flows=1 and base pressure=0
     model.node_decoder.weight.data.zero_()
     model.node_decoder.bias.data.zero_()
-    model.edge_decoder.weight.data.zero_()
-    model.edge_decoder.bias.data.fill_(1.0)
+    model.edge_decoder = ConstantEdgeDecoder([-1.0, 1.0])
     model.tank_indices = torch.tensor([0])
     model.tank_areas = torch.tensor([1.0])
-    model.tank_edges = [torch.tensor([0])]
-    model.tank_signs = [torch.tensor([1.0])]
+    model.tank_edges = [torch.tensor([0,1])]
+    model.tank_signs = [torch.tensor([-1.0, 1.0])]
     X = torch.zeros(1,1,2,2)
     out = model(X, edge_index, edge_attr)
     assert torch.isclose(out['node_outputs'][0,0,0,0], torch.tensor(3.6))
